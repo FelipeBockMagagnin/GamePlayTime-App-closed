6e5fd975408e81066c1cb1a564f805d8ee02bd17
Felpi <felipebmagagnin@gmail.com>
2020-07-01 22:59:39 -0300

    feat: create routes manager

---
+++ src/routes.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+import Games from './pages/Games'
+import { Feather } from '@expo/vector-icons'
+import Login from './pages/Login'
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+export default function Routes () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' options={{ title: 'Login' }} component={Login} />
+        <Stack.Screen name='Home' options={{ headerShown: false }} component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+function TabNavigator () {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          switch (route.name.toLowerCase()) {
+            case 'games':
+              iconName = 'activity'
+              break
+
+            case 'discover':
+              iconName = 'activity'
+              break
+
+            case 'profile':
+              iconName = 'activity'
+              break
+          }
+
+          return <Feather name={iconName} size={size} color={color} />
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray'
+      }}
+    >
+      <Tab.Screen name='Games' component={Games} />
+      <Tab.Screen name='Discover' component={Games} />
+      <Tab.Screen name='Profile' component={Games} />
+    </Tab.Navigator>
+  )
+}
